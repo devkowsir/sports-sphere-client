d3979c7a7786d8d191f6f60c24eb65aaa83d71fc
Kawsar Ahmed <72383564+devkowsir@users.noreply.github.com>
2024-12-04 20:41:23 +0600

    chore: added notification system

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import { AuthProvider } from "./contexts/auth";
 import router from "./routes";
+
+import "react-toastify/ReactToastify.css";
 
 function App() {
   return (
     <AuthProvider>
       <RouterProvider router={router} />
+      <ToastContainer position="bottom-right" />
     </AuthProvider>
   );
 }
